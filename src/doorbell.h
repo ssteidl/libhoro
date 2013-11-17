766a6fb302a217f061c7561a01f4952c88f68919
shane <shane>
2013-11-17 18:46:21 +0000

    Started code cleanup and documentation

--- src/doorbell.h
+++ src/doorbell.h
@@ -21,6 +21,14 @@ typedef enum
 
 #define DBELL_ASTERISK (uint64_t)0xFFFFFFFFFFFFFFFF
 
+/** 
+ * An instance of this structure must be filled in with the current
+ * time values and passed to dbell_process().  dbell_process() will *
+ * use these values to check if the clock has any existing * actions to
+ * be called.
+ *
+ * @see cronprint.c 
+ */
 struct dbell_time
 {
     int minute;
@@ -29,20 +37,42 @@ struct dbell_time
     int month;
     int dayOfWeek;
 };
-
 typedef struct dbell_time dbell_time_t;
 
+/**
+ * Type definition for an action callback.  Actions
+ * are linked to a dbell_clock_t and scheduled for
+ * execution using the dbell_scheduleAction() function.
+ */
 typedef void (*dbell_actionFunc)(void* actionData);
 
+/**
+ * Opaque data structure used to manage actions and their
+ * schedules.
+ */
 typedef struct dbell_clock dbell_clock_t;
 
+/**
+ * Initialize the library.
+ *
+ * @param[out] oClock The address of a pointer that will be made to
+ * point to an initialized dbell_clock.  The dbell_clock's 
+ * resources must be returned to the system using dbell_destroy
+ * after the dbell_clock is no longer needed.
+ */
 DBELL_ERROR
 dbell_init(dbell_clock_t** oClock);
 
+/**
+ * Schedule an action to be executed as described in the
+ * schedule string.
+ *
+ * //TODO: Finish here.
+ */
 DBELL_ERROR
 dbell_scheduleAction(dbell_clock_t* clock, const char *scheduleString, 
                      dbell_actionFunc action, void *actionData,
-                     int* alarmID);
+                     int* oActionID);
                      
 DBELL_ERROR
 dbell_process(dbell_clock_t* clock, dbell_time_t const* timeVals);
