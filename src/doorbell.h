d57f441d60dc7650998fe3d89b5377a25262c903
shane <shane>
2013-12-12 05:16:59 +0000

    Added parameter documentation to scheduleAction and unscheduleAction in doorbell.h

--- src/doorbell.h
+++ src/doorbell.h
@@ -25,8 +25,8 @@ typedef enum
 
 /** 
  * An instance of this structure must be filled in with the current
- * time values and passed to dbell_process().  dbell_process() will *
- * use these values to check if the clock has any existing * actions to
+ * time values and passed to dbell_process().  dbell_process() will 
+ * use these values to check if the clock has any existing actions to
  * be called.
  *
  * @see cronprint.c 
@@ -66,24 +66,45 @@ DBELL_ERROR
 dbell_init(dbell_clock_t** oClock);
 
 /**
- * Schedule an action to be executed as described in the
+ * Schedule an action to be executed periodically as described in the
  * schedule string.
  *
- * //TODO: Finish here.
+ * @param[in] clock A clock structure to which the action will 
+ * be attached.
+ *
+ * @param[in] scheduleString The cron based schedule string which
+ * describes when the action will be executed.
+ *
+ * @param[in] action The action callback function.
+ *
+ * @param[in] Data to be passed to the action when it is executed.
+ *
+ * @param[out] oActionID The id of the action so that it can be
+ * unscheduled if necessary. 
  */
 DBELL_ERROR
 dbell_scheduleAction(dbell_clock_t* clock, const char *scheduleString, 
                      dbell_actionFunc action, void *actionData,
                      int* oActionID);
-                     
-DBELL_ERROR
-dbell_process(dbell_clock_t* clock, dbell_time_t const* timeVals);
 
+/**
+ * Unschedule an action.
+ *
+ * @param[in] clock The clock structure that contains the action.
+ *
+ * @param[in] actionID The actionID from dbell_scheduleAction.
+ */                     
 DBELL_ERROR
 dbell_unscheduleAction(dbell_clock_t* clock, int actionID);
 
+/**
+ *
+ */
 DBELL_ERROR
 dbell_actionCount(dbell_clock_t* clock, int* oActionCount);
+
+DBELL_ERROR
+dbell_process(dbell_clock_t* clock, dbell_time_t const* timeVals);
 
 DBELL_ERROR
 dbell_destroy(dbell_clock_t* clock);
