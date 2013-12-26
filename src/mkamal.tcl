41886690183ea4f8846c17405c904f868e4ea251
ssteidley <ssteidley>
2013-12-26 07:19:56 +0000

    Implemented removing of #line macros from amalgamation. [25e442fc56]

--- src/mkamal.tcl
+++ src/mkamal.tcl
@@ -3,16 +3,43 @@
 # The author disclaims copyright to this source code.
 #
 
+set amalFileName "horo-amal.c"
+
 set files [list horo.h cron.h Parser.h Parser.c cron.c lex.horo.c \
                horo.c]
 
-set amalout [open "horo-amal.c" [list WRONLY CREAT]];
+#Cat the files together
+proc createAmal {} {
+    global amalFileName
+    global files
 
-foreach file $files {
+    set amalout [open $amalFileName [list WRONLY CREAT]];
 
-    set fd [open $file RDONLY]
-    fcopy $fd $amalout
-    close $fd
+    foreach file $files {
+
+	set fd [open $file RDONLY]
+	fcopy $fd $amalout
+	close $fd
+    }
+
+    close $amalout
 }
 
-close $amalout
+#Removes all of the "#line <num> <file>" macros since they
+#no longer apply
+proc removeLineDeclarations {} {
+    global amalFileName
+
+    set amalFD [open $amalFileName "r+"]
+    set amalString [read $amalFD]
+    set scrubbedString [regsub -expanded -all -line {\#line.*$} "$amalString" {}]
+    close $amalFD
+
+    set amalFD [open $amalFileName "w"]
+    puts $amalFD $scrubbedString
+    fconfigure $amalFD -encoding binary
+    close $amalFD
+}
+
+createAmal
+removeLineDeclarations
